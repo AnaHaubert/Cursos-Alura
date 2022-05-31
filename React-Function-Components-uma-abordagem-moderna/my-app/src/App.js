0262f5f27f1053238ac5e87dcab1b96117b6f51b
Ana <anachaubertf@gmail.com>
2022-05-31 09:16:06 +0100

    Exercício e certificado do curso React Function Components uma abordagem moderna

---
+++ React-Function-Components-uma-abordagem-moderna/my-app/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import {Container, Typography} from '@mui/material';
+import '@fontsource/roboto/300.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Container component='article' maxWidth='sm'>
+        <Typography variant='h3' component='h1' align='center'>Formulário de Cadastro</Typography>
+        <FormularioCadastro aoEnviar={aoEnviarForm} validarCPF={validarCPF} />
+      </Container>
+    );
+  }
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
+}
+
+function validarCPF(cpf) {
+  if(cpf.length !== 11) {
+    return {valido: false, texto:'O CPF deve ter 11 dígitos.'}
+  } else {
+    return {valido: true, texto:''}
+  }
+}
+
+export default App;
