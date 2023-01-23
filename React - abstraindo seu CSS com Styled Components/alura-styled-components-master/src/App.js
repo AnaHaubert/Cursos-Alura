9dea62cf4a278bf6f890471796ef0254d85f8c9e
Ana <anachaubertf@gmail.com>
2023-01-23 10:24:22 +0000

    Conteudo cursos SASS-CSS sintaticamente espetacular e React-abstraindo seu CSS com Styled Components

---
+++ React - abstraindo seu CSS com Styled Components/alura-styled-components-master/src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+
+import Cabecalho from "./Components/Cabecalho";
+import Container from "./Components/Container";
+
+import { GlobalStyle } from "./Components/GlobalStyle";
+import SwitcherTema from "./Components/SwitcherTema";
+import { BtnTema } from "./Components/UI";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
+
+function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema((tema) => !tema);
+  }
+
+  return (
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
+      <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherTema tema={tema} />
+      </BtnTema>
+      <Cabecalho />
+      <Container />
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ React - abstraindo seu CSS com Styled Components/alura-styled-components-master/src/Components/Extrato/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Botao } from '../../Components/UI';
+import { extratoLista } from "../../info";
+import Itens from "../Itens";
+
+
+const Extrato = () => {
+    return (
+        <Box>
+            {
+                extratoLista.updates.map(({ id, type, from, value, date }) => {
+                    return (
+                        <Itens
+                            key={id}
+                            type={type}
+                            from={from}
+                            value={value}
+                            date={date}
+                        />
+                    )
+                })
+            }
+            <Botao>Ver mais</Botao>
+        </Box>
+    )
+}
+
+export default Extrato;
