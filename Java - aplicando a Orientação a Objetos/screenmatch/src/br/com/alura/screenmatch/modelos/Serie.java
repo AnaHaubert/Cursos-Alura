5b312a0d51789054d3fdffad2d9a128fd5744293
= <anachaubertf@gmail.com>
2023-08-14 18:04:07 +0100

    Incluindo primeiros cursos java

---
+++ Java - aplicando a Orientação a Objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,45 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private boolean ativa;
+    private int episodeosPorTemporada;
+    private int minutosPorEpisodeo;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public int getEpisodeosPorTemporada() {
+        return episodeosPorTemporada;
+    }
+
+    public void setEpisodeosPorTemporada(int episodeosPorTemporada) {
+        this.episodeosPorTemporada = episodeosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodeo() {
+        return minutosPorEpisodeo;
+    }
+
+    public void setMinutosPorEpisodeo(int minutosPorEpisodeo) {
+        this.minutosPorEpisodeo = minutosPorEpisodeo;
+    }
+
+    @Override
+    public int getDuracaoEmMinutos() {
+        return temporadas * episodeosPorTemporada * minutosPorEpisodeo;
+    }
+}
